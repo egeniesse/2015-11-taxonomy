ae9d5867cb496e1e48b8e91f8326dab5c00314c9
Eric Geniesse <egeniesse@gmail.com>
2015-11-30 12:13:29 -0800

    finished the forager bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -2,5 +2,12 @@ var ForagerBee = function() {
 	Bee.call(this);
 	this.age = 10;
 	this.job = 'find pollen';
+	this.canFly = true;
+	this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.forage= function(){
+	this.treasureChest.push('treasure');
+};
+ForagerBee.prototype.constructor = ForagerBee;
